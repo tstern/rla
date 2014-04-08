ae8e46cf39d37d1516e9f1ab560aeb5b1b502fb9
Thomas Stern <thomas.stern@ion2s.com>
2014-04-08 13:56:13 +0200

    - styled info page

--- client/app/scripts/controllers/info.js
+++ client/app/scripts/controllers/info.js
@@ -7,14 +7,43 @@
 			function ($scope, ResourceService) {
 				var laureateId = $scope.$stateParams.id;
 
-				$scope.laureate = null;
+//				$scope.laureate = null;
+				$scope.laureate = {
+					name: 'Amy Goodman',
+					year: 2008,
+					country: 'USA',
+					quote: '... for developing an innovative model of truly indepedent political journalism that brings to millions of people the alternative voices that are often excluded by mainstream media.',
+					description: 'The media is sometimes called the fourth estate in a democracy. But in many countries of the world, the media is today no longer willing or able to play this role. Instead it defers to commercial and political interests, thus eroding democracy. With Democracy Now!, Amy Goodman has shown what the alternative to this dangerous trend can look like. Democracy Now! is the largest public media collaboration in the U.S. which is now available to people seeking alternative viewpoints around the globe.',
+					paragraphs: [
+						{
+							title: 'Career',
+							text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+						},
+						{
+							title: 'Working for a chemical weapon free world',
+							text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+						},
+						{
+							title: 'Promoting environmentally sound pathways for the actual physical destruction of chemical weapons',
+							text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+						},
+						{
+							title: 'A role model for the elimination of all weapons of mass destruction',
+							text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+						},
+						{
+							title: 'Honours',
+							text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+						}
+					]
+				};
 
 				ResourceService.getLaureates().then(function (laureates) {
 					var i;
 
 					for (i = 0; i < laureates.length; i++) {
 						if (laureates[i].id === laureateId) {
-							$scope.laureate = laureates[i];
+//							$scope.laureate = laureates[i];
 						}
 					}
 
