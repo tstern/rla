d695cabfc82b106394e1d9f6f1020d63fe6741ed
Thomas Stern <thomas.stern@ion2s.com>
2014-04-09 16:45:20 +0200

    - implemented image wall

--- client/app/scripts/controllers/wall.js
+++ client/app/scripts/controllers/wall.js
@@ -3,8 +3,8 @@
 
 	angular.module('rla')
 
-		.controller('WallCtrl', ['$scope', 'ResourceService',
-			function ($scope, ResourceService) {
+		.controller('WallCtrl', ['$scope', 'ResourceService', 'HelperService',
+			function ($scope, ResourceService, HelperService) {
 				$scope.laureates = null;
 
 				$scope.neighbours = {
@@ -12,7 +12,153 @@
 					right: 'map'
 				};
 
-				$scope.title = 'Wall';
+				var images = HelperService.groupImagesByHeight(getImages());
+				images = HelperService.reorderImages(images, 2 * 924);
+
+				$scope.images = images;
 			}]);
+
+	function getImages() {
+		return [
+			{
+				name: 'ahalf',
+				width: 400,
+				height: 500,
+				laureateId: 1
+			},
+			{
+				name: 'ambasi',
+				width: 400,
+				height: 500,
+				laureateId: 1
+			},
+			{
+				name: 'aware',
+				width: 400,
+				height: 800,
+				laureateId: 1
+			},
+			{
+				name: 'dkolkov',
+				width: 400,
+				height: 400,
+				laureateId: 1
+			},
+			{
+				name: 'dsuzuki',
+				width: 400,
+				height: 700,
+				laureateId: 1
+			},
+			{
+				name: 'ekraeutler',
+				width: 690,
+				height: 600,
+				laureateId: 1
+			},
+			{
+				name: 'gasamoah',
+				width: 400,
+				height: 400,
+				laureateId: 1
+			},
+			{
+				name: 'gcan',
+				width: 400,
+				height: 300,
+				laureateId: 1
+			},
+			{
+				name: 'gsommer',
+				width: 400,
+				height: 500,
+				laureateId: 1
+			},
+			{
+				name: 'jseven',
+				width: 400,
+				height: 300,
+				laureateId: 1
+			},
+			{
+				name: 'jterry',
+				width: 400,
+				height: 500,
+				laureateId: 1
+			},
+			{
+				name: 'ksmith',
+				width: 700,
+				height: 600,
+				laureateId: 1
+			},
+			{
+				name: 'ldurant',
+				width: 400,
+				height: 700,
+				laureateId: 1
+			},
+			{
+				name: 'mburns',
+				width: 400,
+				height: 700,
+				laureateId: 1
+			},
+			{
+				name: 'nbassey',
+				width: 678,
+				height: 600,
+				laureateId: 1
+			},
+			{
+				name: 'oshiru',
+				width: 400,
+				height: 500,
+				laureateId: 1
+			},
+			{
+				name: 'pcouch',
+				width: 700,
+				height: 600,
+				laureateId: 1
+			},
+			{
+				name: 'pwalker',
+				width: 572,
+				height: 600,
+				laureateId: 1
+			},
+			{
+				name: 'rbrenner',
+				width: 400,
+				height: 400,
+				laureateId: 1
+			},
+			{
+				name: 'rngongo',
+				width: 400,
+				height: 300,
+				laureateId: 1
+			},
+			{
+				name: 'saydhpa',
+				width: 616,
+				height: 600,
+				laureateId: 1
+			},
+			{
+				name: 'supadhyay',
+				width: 666,
+				height: 600,
+				laureateId: 1
+			},
+			{
+				name: 'ttonga',
+				width: 400,
+				height: 300,
+				laureateId: 1
+			}
+		];
+	}
 
 }());
---
+++ client/app/scripts/directives/wallImage.js
@@ -0,0 +1,95 @@
+/**
+ * Created by Thomas on 09.04.2014.
+ */
+(function () {
+
+	angular.module('rla')
+
+		.directive('tsWallImage', ['$rootScope', 'HelperService',
+			function ($rootScope, HelperService) {
+				return {
+					link: function ($scope, $elem, $attrs) {
+						var active = false,
+							element = $elem.get(0),
+							$quote = $elem.find('.quote'),
+							listName = $attrs.tsWallImage,
+							image = $scope[listName][$scope.$index],
+							transformKey = HelperService.determineTransformKey();
+
+						setupStyles();
+
+						function setupStyles() {
+							element.style.zIndex = $scope.$index + 10;
+							$quote.hide();
+
+							if (image.first) {
+								element.style.marginLeft = image.space + 'px';
+							}
+
+							if (image.small) {
+								calculateTopAndBottomSpace(300);
+							}
+
+							if (image.medium) {
+								calculateTopAndBottomSpace(400);
+							}
+
+							if (image.large) {
+								calculateTopAndBottomSpace(550);
+							}
+
+							function calculateTopAndBottomSpace(minHeight) {
+								var margin = ((image.height - minHeight) / -4) + 'px';
+
+								element.style.marginTop = margin;
+								element.style.marginBottom = margin;
+							}
+						}
+
+						$elem.find('img').on('click', function (event) {
+							onImageClick();
+						});
+
+						$rootScope.$on('images:deactivate', function (event, $index) {
+							if ($index !== $scope.$index) {
+								deactivateImage();
+							}
+						});
+
+						function onImageClick() {
+							deactivateAllImages();
+
+							if (!active) {
+								activateImage();
+							} else {
+								deactivateImage();
+							}
+						}
+
+						function activateImage() {
+							active = true;
+							element.style[transformKey] = 'scale(2)';
+							element.style.boxShadow = '0px 0px 20px 5px #007dbf';
+							element.style.zIndex = $scope[listName].length + 10;
+							$quote.show();
+						}
+
+						function deactivateImage() {
+							active = false;
+							element.style[transformKey] = '';
+							element.style.boxShadow = '';
+							element.style.zIndex = $scope.$index + 10;
+							$quote.hide();
+						}
+
+						function deactivateAllImages() {
+							$rootScope.$emit('images:deactivate', $scope.$index);
+						}
+
+						$scope.$on('$destroy', function (event) {
+							$elem.off('click');
+						});
+					}
+				};
+			}]);
+}());
